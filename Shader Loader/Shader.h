8f4ccd511c9b34f6656f218ff53e7a692c3f365d
Stefan Papuc <131554928+pUmA73@users.noreply.github.com>
2025-02-28 23:05:23 +0200

    Create Shader.h

---
+++ Shader Loader/Shader.h
@@ -0,0 +1,48 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <fstream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader
+{
+public:
+	// the program ID
+	unsigned int ID;
+
+	// constructor that reads and builds the shader
+	Shader(const char* vertexPath, const char* fragmentPath, const char* tessControlPath = nullptr, const char* tessEvalPath = nullptr);
+
+	// use/activate the shader
+	void use();
+
+	// utility uniform functions
+	void setBool(const std::string& name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+	
+	void setVec2(const std::string& name, const glm::vec2& value) const;
+	void setVec2(const std::string& name, float x, float y) const;
+	void setVec3(const std::string& name, const glm::vec3& value) const;
+	void setVec3(const std::string& name, float x, float y, float z) const;
+	void setVec4(const std::string& name, const glm::vec4& value) const;
+	void setVec4(const std::string& name, float x, float y, float z, float w) const;
+
+	void setMat2(const std::string& name, const glm::mat2& mat) const;
+	void setMat3(const std::string& name, const glm::mat3& mat) const;
+	void setMat4(const std::string& name, const glm::mat4& mat) const;
+
+private:
+	// utility function for checking shader compilation/linking errors
+	void checkCompileErrors(unsigned int shader, std::string type);
+};
+
+#endif
